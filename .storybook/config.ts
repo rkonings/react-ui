244b1dd71d15af485934fa867f1b0096fc391df3
rkonings <randy@randykonings.nl>
2019-07-14 14:59:18 +0200

    setup storybook addon knobs

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,6 +1,8 @@
 import { configure, addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
 import ThemeProviderDecorator from './ThemeProviderDecorator';
 import CenterStoryDecorator from './CenterStoryDecorator';
+
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../stories', true, /\.stories\.tsx$/);
 
@@ -8,7 +10,7 @@ function loadStories() {
   req.keys().forEach(req);
 }
 
-
+addDecorator(withKnobs);
 addDecorator(ThemeProviderDecorator);
 addDecorator(CenterStoryDecorator);
 
