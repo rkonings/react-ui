cd97cb44d627fd9b32235090917e0a3824737627
rkonings <randy@randykonings.nl>
2019-07-31 20:03:19 +0200

    add index for icons

---
+++ src/Icon/index.ts
@@ -0,0 +1,29 @@
+export { default as ArrowLeft } from './ArrowLeft';
+export { default as ArrowRight } from './ArrowRight';
+export { default as ArrowUp } from './ArrowUp';
+export { default as ArrowDown } from './ArrowDown';
+
+export { default as AngleDoubleDown } from './AngleDoubleDown';
+export { default as AngleDoubleUp } from './AngleDoubleUp';
+export { default as AngleDoubleLeft } from './AngleDoubleLeft';
+export { default as AngleDoubleRight } from './AngleDoubleRight';
+
+export { default as AngleDown } from './AngleDown';
+export { default as AngleUp } from './AngleUp';
+export { default as AngleLeft } from './AngleLeft';
+export { default as AngleRight } from './AngleRight';
+
+export { default as ChevronUp } from './ChevronUp';
+export { default as ChevronDown } from './ChevronDown';
+export { default as ChevronLeft } from './ChevronLeft';
+export { default as ChevronRight } from './ChevronRight';
+
+export { default as CaretUp } from './CaretUp';
+export { default as CaretDown } from './CaretDown';
+export { default as CaretLeft } from './CaretLeft';
+export { default as CaretRight } from './CaretRight';
+
+export { default as Plus } from './Plus';
+export { default as Minus } from './Minus';
+
+export { default as Check } from './Check';
