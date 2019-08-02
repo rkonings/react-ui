15be84e082827e8472d38d036e0055972cbfa650
rkonings <randy@randykonings.nl>
2019-08-02 12:36:10 +0200

    add IconSpacing mixin and add width and height to SVGIcon mixin

--- src/mixins/SVGIcon.ts
+++ src/mixins/SVGIcon.ts
@@ -1,5 +1,21 @@
-import Theme from '../interfaces/Theme';
+import IconProps from '../interfaces/IconProps';
+import DefaultTheme from '../themes/default';
 
-export const SVGIcon = ({theme, type = 'default'}: {theme: Theme, type: string}) => `
+export const IconSpacing = ({spacing}: IconProps) => {
+  if (spacing === 'top') {
+    return `margin-top: 1em`;
+  } else if (spacing === 'right') {
+    return `margin-right: 1em`;
+  } else if (spacing === 'bottom') {
+    return `margin-bottom: 1em`;
+  } else if (spacing === 'left') {
+    return `margin-left: 1em`;
+  }
+  return;
+};
+
+export const SVGIcon = ({theme = DefaultTheme, type = 'default', size}: IconProps) => `
   fill: ${theme.icon[type] || theme.icon.default};
+  width: ${size || theme.icon.defaultSize}px;
+  height: ${size || theme.icon.defaultSize}px;
 `;
