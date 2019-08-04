5d06be14b1f714e174d84b919a50366a7a0bb5c5
rkonings <randy@randykonings.nl>
2019-08-04 09:35:31 +0200

    add icon size to theme

--- src/themes/icon.ts
+++ src/themes/icon.ts
@@ -4,7 +4,13 @@ const icon = {
     default: color.gray130,
     primary: color.blue60,
     secondairy: color.orange60,
-    defaultSize: 40
+    size: {
+        xs: 10,
+        s: 20,
+        m: 30,
+        l: 40,
+        xl: 50
+    }
 };
 
 export default icon;
