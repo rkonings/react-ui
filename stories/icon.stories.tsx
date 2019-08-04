9f642f1e072ca4ae278f14b967ba91663bb95376
rkonings <randy@randykonings.nl>
2019-08-04 09:44:21 +0200

    add knob for type and size for icon stories

--- stories/icon.stories.tsx
+++ stories/icon.stories.tsx
@@ -1,15 +1,29 @@
+import { select } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
+import icon from '../src/themes/icon';
 
 import * as Icons from '../src/Icon';
 
 const IconStories = storiesOf('Icon', module);
 
+const typeLabel = 'Colors';
+const typeOptions = {
+  Default: 'default',
+  Primary: 'primary',
+  Secondairy: 'secondairy'
+};
+const typeDefaultValue = 'default';
+
 Object.keys(Icons).forEach((Icon) => {
   IconStories.add(Icon, () => {
     const ComponentName = Icons[Icon];
+    const type = select(typeLabel, typeOptions, typeDefaultValue);
+
+    const sizeOptions = Object.keys(icon.size);
+    const size = select('Size', sizeOptions, 'm');
     return (
-      <ComponentName />
+      <ComponentName type={type} size={size} />
     );
   });
 });
