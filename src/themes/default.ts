bdf4d4fed3f5c018ec13f68435214116c7a3c005
rkonings <randy@randykonings.nl>
2019-08-04 09:25:28 +0200

    add icon properties to theme

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -2,11 +2,13 @@ import Theme from '../interfaces/Theme';
 
 import button from './buttons';
 import color from './colors';
+import icon from './icon';
 import table from './table';
 
 const theme: Theme = {
     fontFamily: 'Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
     color,
+    icon,
     button,
     table,
     loader: {
---
+++ src/themes/icon.ts
@@ -0,0 +1,10 @@
+import color from './colors';
+
+const icon = {
+    default: color.gray130,
+    primary: color.blue60,
+    secondairy: color.orange60,
+    defaultSize: 40
+};
+
+export default icon;
