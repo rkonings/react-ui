f6e0d12fe230a866f3fa839713f0d35526edfad0
rkonings <randy@randykonings.nl>
2019-08-10 09:49:34 +0200

    add element type interface
    with default | primary | secondairy

--- src/interfaces/Theme.ts
+++ src/interfaces/Theme.ts
@@ -1,3 +1,4 @@
 export type Size = 'xs' | 'sm' | 'm' | 'l' | 'xl';
+export type Type = 'default' | 'primary' | 'secondairy';
 // tslint:disable-next-line:no-any
 export default interface Theme { [s: string]: any; }
