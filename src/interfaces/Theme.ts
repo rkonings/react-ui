66184936153d0037bad4ff52c295eeb1a0c2d19d
rkonings <randy@randykonings.nl>
2019-08-11 10:28:46 +0200

    fix wrong size sm to s

--- src/interfaces/Theme.ts
+++ src/interfaces/Theme.ts
@@ -1,4 +1,4 @@
-export type Size = 'xs' | 'sm' | 'm' | 'l' | 'xl';
+export type Size = 'xs' | 's' | 'm' | 'l' | 'xl';
 export type Type = 'default' | 'primary' | 'secondairy';
 // tslint:disable-next-line:no-any
 export default interface Theme { [s: string]: any; }
