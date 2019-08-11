4c98e758c6874db6639580d0cd3728d42606306d
rkonings <randy@randykonings.nl>
2019-08-11 15:06:52 +0200

    fix javascript error onClick is not a function

--- src/ButtonGroup/ButtonGroup.tsx
+++ src/ButtonGroup/ButtonGroup.tsx
@@ -24,7 +24,7 @@ const ButtonGroup = ({className, children, type, size, outline}: ButtonGroupProp
             outline,
             onClick: () => {
                 setActiveIndex(index);
-                child.props.onClick();
+                if (child.props.onClick) { child.props.onClick(); }
             },
             active: (activeIndex === index)
           });
