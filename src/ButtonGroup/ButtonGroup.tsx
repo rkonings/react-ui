6987bbb16146f33fd0f511694f6f0a2f94c49b67
rkonings <randy@randykonings.nl>
2019-08-13 15:38:43 +0200

    refactor: reove unused outline prop

--- src/ButtonGroup/ButtonGroup.tsx
+++ src/ButtonGroup/ButtonGroup.tsx
@@ -8,21 +8,19 @@ type ButtonGroupType = 'default' | 'primary' | 'secondairy';
 interface ButtonGroupProps {
     size?: ButtonGroupSize;
     type?: ButtonGroupType;
-    outline?: boolean;
     theme?: Theme;
     className?: string;
     setActive?: boolean;
     children: JSX.Element | JSX.Element[];
 }
 
-const ButtonGroup = ({setActive = false, className, children, type, size, outline}: ButtonGroupProps) => {
+const ButtonGroup = ({setActive = false, className, children, type, size}: ButtonGroupProps) => {
 
     const [activeIndex, setActiveIndex] = React.useState<number>(-1);
     const buttons = React.Children.map(children, (child, index) => {
         return React.cloneElement<ButtonProps>(child, {
             type: child.props.type || type,
             size,
-            outline,
             onClick: (event) => {
                 if (setActive) {setActiveIndex(index); }
                 if (child.props.onClick) { child.props.onClick(event); }
@@ -39,15 +37,7 @@ const StyledButtonGroup = styled(ButtonGroup)`
     align-items: center;
 
     ${Button} {
-        ${({outline, theme: { buttonGroup }, type = 'default'}) => {
-            if (outline) {
-                return `
-                    border-right-width: 0;
-                    &:last-child {
-                        border-right-width: 1px;
-                    }
-                `;
-            }
+        ${({theme: { buttonGroup }, type = 'default'}) => {
             return `
                 border-width: 0px;
                 border-color: ${buttonGroup[type].borderColor};
