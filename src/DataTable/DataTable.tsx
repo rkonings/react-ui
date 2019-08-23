395b2233f580c3dd80fd6ba0a075f4a31c2c0f43
rkonings <randy@randykonings.nl>
2019-08-23 17:20:51 +0200

    refactor getColumnWidth

--- src/DataTable/DataTable.tsx
+++ src/DataTable/DataTable.tsx
@@ -181,6 +181,21 @@ const StyledHeader = styled(Header)`
     display: flex;
 `;
 
+const getColumnWidthByType = (column: Column): number => {
+    if (column.type === 'SELECT') {
+        return 50;
+    }
+
+    const width = column.width;
+    if (typeof width === 'undefined') {
+        return 100;
+    } else if (typeof width === 'string') {
+        return Number(width);
+    }
+
+    return width;
+};
+
 const DataTable = ({data, fields, className, columns}: DataTableProps) => {
 
     const [selectAll, setSelectAll] = useState(false);
@@ -209,16 +224,8 @@ const DataTable = ({data, fields, className, columns}: DataTableProps) => {
 
     const getColumnWidth = memoize((index: number): number => {
         const column = columns[index];
-        if (column.type === 'SELECT') {
-            return 50;
-        }
-
-        const width = column.width;
-        if (typeof width === 'undefined') {
-            return 100;
-        } else if (typeof width === 'string') {
-            return Number(width);
-        }
+        return getColumnWidthByType(column);
+    });
 
         return width;
     });
@@ -248,7 +255,7 @@ const DataTable = ({data, fields, className, columns}: DataTableProps) => {
                 rowHeight={() => 40}
                 rowCount={data.length}
                 itemData={itemData}
-                width={800}
+                width={getTableWidth(columns)}
                 height={600}
                 overscanCount={10}
             >
