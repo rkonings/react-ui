f9f4cd1fdeb964d301337ef06ccdb4dc0337dcbc
rkonings <randy@randykonings.nl>
2019-08-27 21:10:50 +0200

    add basic create account form

--- stories/login.stories.tsx
+++ stories/login.stories.tsx
@@ -4,12 +4,19 @@ import { boolean, select, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
+import CreateAccount from '../src/UI/CreateAccount';
 import Login from '../src/UI/Login';
 
-storiesOf('UI/Login', module)
+storiesOf('UI/Forms', module)
 .add('login', () => {
 
   return (
     <Login onLogin={action('login')}/>
+  );
+})
+.add('Create account', () => {
+
+  return (
+    <CreateAccount onCreate={action('Create account')}/>
   );
 });
