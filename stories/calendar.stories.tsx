ad99844c71091ad6c809625bc63dd447f05bb727
rkonings <randy@randykonings.nl>
2019-09-11 10:32:09 +0200

    add story with daterange support

--- stories/calendar.stories.tsx
+++ stories/calendar.stories.tsx
@@ -8,6 +8,28 @@ import React from 'react';
 import Calendar from '../src/Calendar/Calendar';
 
 storiesOf('Calendar', module)
+.add('with daterange', () => {
+    const options = {
+        range: true,
+        min: 2000,
+        max: 2028,
+        step: 1,
+    };
+
+    const range = {
+        start: moment([2019, 0, 1]),
+        end: moment([2019, 0, 14])
+    };
+
+    return (
+        <Calendar
+            onChange={action('Change')}
+            startYear={number('Start year', 2018, options)}
+            endYear={number('End year', 2028, options)}
+            value={range}
+        />
+    );
+})
 .add('default', () => {
     const options = {
         range: true,
