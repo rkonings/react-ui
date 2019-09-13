fbfecc8e9110d6d5c181386f633545fc34629ea0
rkonings <randy@randykonings.nl>
2019-09-13 07:43:51 +0200

    refactor: move month component to own file

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -10,60 +10,8 @@ import { ArrowLeft, ArrowRight, CaretDown } from '../Icon/index';
 import DateInput from '../Input/DateInput/DateInput';
 import DateRangeInput from '../Input/DateInput/DateRangeInput';
 import { DateRange } from '../interfaces/Date';
+import Month from './Month';
 import { MonthSelect } from './MonthSelect';
-import Week from './Week';
-
-const MonthTitle = styled.div`
-    width: 100%;
-    text-transform: uppercase;
-    font-size: 14px;
-`;
-
-interface Month {
-    className?: string;
-    month: number;
-    year: number;
-    value: moment.Moment | DateRange;
-    potentialRange?: DateRange | null;
-    onChangePotentialRange?(date: moment.Moment): void;
-    onChange(year: number, month: number, day: number): void;
-}
-
-const Month = styled(({className, month, year, value,
-    potentialRange, onChangePotentialRange, onChange}: Month) => {
-    const firstDayOfMonth = moment([year, month]).startOf('month');
-    const weeksInMonth = Math.ceil((firstDayOfMonth.isoWeekday() + firstDayOfMonth.daysInMonth()) / 7);
-    const weeks = [];
-
-    for (let i = 0; i < weeksInMonth; i++ ) {
-        weeks.push(
-            <Week
-                isoWeek={firstDayOfMonth.clone().startOf('isoWeek')}
-                value={value}
-                month={month}
-                year={year}
-                onChange={onChange}
-                potentialRange={potentialRange}
-                onChangePotentialRange={onChangePotentialRange}
-            />
-        );
-
-        firstDayOfMonth.add(7, 'days');
-    }
-
-    return (
-        <div className={className}>
-            <MonthTitle>{moment([year, month]).format('MMM YYYY')}</MonthTitle>
-            {weeks}
-        </div>
-    );
-})`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    margin-bottom: 25px;
-`;
 
 interface Calendar {
     className?: string;
