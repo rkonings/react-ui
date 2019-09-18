6bdb8c0a62d8429dfbeef1f8a1d4fa329eb9a0cf
rkonings <randy@randykonings.nl>
2019-09-18 19:45:38 +0200

    add button menu

--- stories/appBar.stories.tsx
+++ stories/appBar.stories.tsx
@@ -4,6 +4,8 @@ import * as React from 'react';
 import AppBar, { AppBarTitle } from '../src/AppBar/AppBar';
 import { TextButton } from '../src/Button';
 import { Bars, Search } from '../src/Icon';
+import ButtonMenu from '../src/Menu/ButtonMenu';
+import { MenuItem } from '../src/Menu/Menu';
 
 storiesOf('App bar', module)
 .add('default', () => {
@@ -11,7 +13,20 @@ storiesOf('App bar', module)
         <AppBar>
             <AppBarTitle>App title</AppBarTitle>
             <TextButton type="light" isIcon={true}><Search /></TextButton>
-            <TextButton type="light" isIcon={true}><Bars /></TextButton>
+            <ButtonMenu
+                type="light"
+                variant="text"
+                menuAlign="RIGHT"
+                items={ (close) => (
+                    <React.Fragment>
+                        <MenuItem onClick={() => close()}>Profile</MenuItem>
+                        <MenuItem onClick={() => close()}>Settings</MenuItem>
+                        <MenuItem onClick={() => close()}>Sign out</MenuItem>
+                    </React.Fragment>
+                )}
+            >
+                Menu
+            </ButtonMenu>
         </AppBar>
     );
 });
