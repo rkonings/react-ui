44f4949577ef5b7382734499fd0119e795768d2d
rkonings <randy@randykonings.nl>
2019-09-18 19:22:43 +0200

    Add TextButton with icons

--- stories/appBar.stories.tsx
+++ stories/appBar.stories.tsx
@@ -2,12 +2,16 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
 import AppBar, { AppBarTitle } from '../src/AppBar/AppBar';
+import { TextButton } from '../src/Button';
+import { Bars, Search } from '../src/Icon';
 
 storiesOf('App bar', module)
 .add('default', () => {
     return (
         <AppBar>
             <AppBarTitle>App title</AppBarTitle>
+            <TextButton type="light" isIcon={true}><Search /></TextButton>
+            <TextButton type="light" isIcon={true}><Bars /></TextButton>
         </AppBar>
     );
 });
