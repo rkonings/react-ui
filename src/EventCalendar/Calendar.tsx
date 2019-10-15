c0df7f1dba72bab7f77e96be8f8a1952a1f3f153
rkonings <randy@randykonings.nl>
2019-10-15 10:14:25 +0200

    remove events from component

--- src/EventCalendar/Calendar.tsx
+++ src/EventCalendar/Calendar.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { Event } from './interfaces';
 import Month from './Month';
 
 interface Calendar {
     className?: string;
-    events: Event[];
+    year: number;
+    month: number;
 }
 
-export const Calendar = ({className, events}: Calendar) => {
 export const Calendar = ({className, year, month}: Calendar) => {
     return (
         <div className={className}>
