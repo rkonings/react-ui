c1b637d5c6fe6e91e521e67ccf99c9d9ee526eb4
rkonings <randy@randykonings.nl>
2019-10-25 16:17:24 +0200

    add Company component

---
+++ src/UI/InvoiceBuilder/Company.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import { CompanyData } from './interfaces';
+
+interface Company {
+    className?: string;
+    data: CompanyData;
+}
+
+const Client = ({className, data}: Company) => {
+
+    const { name, address, zipcode, city, email, phone, website, vatNumber, bankAccount, coc } = data;
+
+    return (
+        <div className={className}>
+            <ul>
+                <li>{name}</li>
+                <li>{address}</li>
+                <li>{zipcode}</li>
+                <li>{city}</li>
+            </ul>
+
+            <ul>
+                <li>email: {email}</li>
+                <li>phone: {phone}</li>
+                <li>website: {website}</li>
+            </ul>
+
+            <ul>
+                <li>vat number: {vatNumber}</li>
+                <li>bank account: {bankAccount}</li>
+                <li>C.o.C number: {coc}</li>
+            </ul>
+        </div>
+    );
+};
+
+export default styled(Client)`
+    color: ${({theme: { color }}) => color.gray90};
+    ul {
+        list-style: none;
+        margin-left:0;
+        padding-left:0;
+        font-size: 12px;
+    }
+`;
