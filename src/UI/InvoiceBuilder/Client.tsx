a53d6068afbd8cfb394fd3b0cf5c74336db7c561
rkonings <randy@randykonings.nl>
2019-10-25 16:17:13 +0200

    add Client component

---
+++ src/UI/InvoiceBuilder/Client.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ClientData } from './interfaces';
+
+interface Client {
+    className?: string;
+    data: ClientData;
+}
+
+const Client = ({className, data}: Client) => {
+
+    const { firstName, lastName, companyName, address, zipcode, city } = data;
+
+    return (
+        <div className={className}>
+            <ul>
+                <li>{firstName} {lastName}</li>
+                <li>{companyName}</li>
+                <li>{address}</li>
+                <li>{zipcode}</li>
+                <li>{city}</li>
+            </ul>
+        </div>
+    );
+};
+
+export default styled(Client)`
+    color: ${({theme: { color }}) => color.gray90};
+    ul {
+        list-style: none;
+        margin-left:0;
+        padding-left:0;
+        font-size: 12px;
+    }
+`;
