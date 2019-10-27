0f8095581e8dfb7129b8e5fa7033a4f468583fe6
rkonings <randy@randykonings.nl>
2019-10-27 08:23:38 +0100

    add client, company, logo and invoiceinfo components

--- src/UI/InvoiceBuilder/InvoiceBuilder.tsx
+++ src/UI/InvoiceBuilder/InvoiceBuilder.tsx
@@ -1,20 +1,78 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ItemData } from './interfaces';
+import { Grid, Item } from '../../Grid';
+import Client from './Client';
+import Company from './Company';
+import { ClientData, CompanyData, ItemData } from './interfaces';
 import Invoice from './Invoice';
+import InvoiceInfo from './InvoiceInfo';
 
 interface InvoiceBuilder {
     className?: string;
     items: ItemData[];
+    client: ClientData;
+    company: CompanyData;
+    logo: string;
+    dueDate: string;
+    date: string;
+    invoiceNumber: string;
 }
-const InvoiceBuilder = ({className, items}: InvoiceBuilder) => {
+
+const CompanyLogo = styled.img`
+    margin-bottom: 50px;
+    max-width: 100%;
+`;
+
+const InvoiceBuilder = ({className, items, client, company, logo,
+    dueDate, date, invoiceNumber}: InvoiceBuilder) => {
     return (
         <div className={className}>
+            <Grid width="100%">
+                <Item width="50%">
+                    <CompanyLogo src={logo} />
+                </Item>
+                <Item horizontalAlignment="flex-end" width="50%">
+                    <Company data={company} />
+                </Item>
+            </Grid>
+
+            <Grid width="100%">
+                <Item width="50%">
+                    <Client data={client} />
+                </Item>
+                <Item width="50%" horizontalAlignment="flex-end">
+                    <InvoiceInfo
+                        dueDate={dueDate}
+                        invoiceNumber={invoiceNumber}
+                        date={date}
+                    />
+                </Item>
+            </Grid>
+
             <Invoice items={items} />
         </div>
     );
 };
 
 export default styled(InvoiceBuilder)`
+    display:flex;
+    width: 800px;
+    flex-wrap: wrap;
 
+    ${Invoice} {
+        margin-top: 50px;
+    }
+
+    ${InvoiceInfo} {
+        text-align: right;
+    }
+
+    ${Client} {
+        width: 50%;
+    }
+
+    ${Company} {
+        width: 50%;
+        text-align: right;
+    }
 `;
