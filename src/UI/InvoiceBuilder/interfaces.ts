986d845aa4cea0d900b5004251c4296e0ecbe6b6
rkonings <randy@randykonings.nl>
2019-10-27 08:24:30 +0100

    add properties to companyData interface

--- src/UI/InvoiceBuilder/interfaces.ts
+++ src/UI/InvoiceBuilder/interfaces.ts
@@ -1,13 +1,3 @@
-export interface CompanyData {
-    name: string;
-    address: string;
-    zipcode: string;
-    city: string;
-    bankAcccount: string;
-    coc: string;
-    taxNumber: string;
-}
-
 export interface PersonData {
     firstName: string;
     lastName: string;
@@ -31,3 +21,15 @@ export interface ClientData {
     city: string;
 }
 
+export interface CompanyData {
+    name: string;
+    address: string;
+    zipcode: string;
+    city: string;
+    email: string;
+    phone: string;
+    website: string;
+    vatNumber: string;
+    bankAccount: string;
+    coc: string;
+}
