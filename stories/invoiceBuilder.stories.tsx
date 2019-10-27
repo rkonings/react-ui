5dce776bda1eec566cee6b13f9fa20fbdeedf412
rkonings <randy@randykonings.nl>
2019-10-27 08:21:42 +0100

    add client, company, due date, invoice date and logo

--- stories/invoiceBuilder.stories.tsx
+++ stories/invoiceBuilder.stories.tsx
@@ -1,9 +1,33 @@
 import { storiesOf } from '@storybook/react';
+import faker from 'faker';
 import React from 'react';
 import { InvoiceBuilder } from '../src/UI/InvoiceBuilder';
+import { CompanyData } from '../src/UI/InvoiceBuilder/interfaces';
 
 storiesOf('Invoice builder', module)
 .add('default', () => {
+
+    const client = {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        companyName: faker.company.companyName(),
+        address: faker.address.streetAddress(),
+        zipcode: faker.address.zipCode(),
+        city: faker.address.city(),
+    };
+
+    const company: CompanyData = {
+        name: faker.company.companyName(0),
+        address: faker.address.streetAddress(),
+        zipcode: faker.address.zipCode(),
+        city: faker.address.city(),
+        phone: faker.phone.phoneNumber(),
+        email: faker.internet.email(),
+        website: faker.internet.domainName(),
+        vatNumber: faker.finance.account(),
+        bankAccount: faker.finance.account(),
+        coc: faker.finance.account()
+    };
 
     const items = [
         {
@@ -27,6 +51,14 @@ storiesOf('Invoice builder', module)
     ];
 
     return (
-        <InvoiceBuilder items={items} />
+        <InvoiceBuilder
+            client={client}
+            items={items}
+            company={company}
+            dueDate="2014-06-12"
+            date="2019-10-10"
+            invoiceNumber={faker.finance.account(10)}
+            logo={'https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.png'}
+        />
     );
 });
