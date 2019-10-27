7f723e21f842713014308adf15487dbedcdfc8d3
rkonings <41450820+rkonings@users.noreply.github.com>
2019-10-27 15:49:14 +0100

    feat: add font smoothing (#53)

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -4,6 +4,8 @@ const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
     body {
         font-family: 'Roboto', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
     }
 `;
 
