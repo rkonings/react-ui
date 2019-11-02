78ec798be82a6b6ca939c5d4a9642c459c56a6aa
rkonings <randy@randykonings.nl>
2019-11-02 09:22:46 +0100

    add layout stories

---
+++ stories/layout.stories.tsx
@@ -0,0 +1,133 @@
+import { storiesOf } from '@storybook/react';
+import arraySort from 'array-sort';
+import faker from 'faker/locale/nl';
+import React from 'react';
+import styled from 'styled-components';
+import { Basic } from '../src/Layout';
+
+import ButtonGroup from '../src/ButtonGroup/ButtonGroup';
+import { DataTable } from '../src/DataTable';
+import RowAction from '../src/DataTable/DataTableRowAction';
+import { Edit, Options, Trash } from '../src/Icon';
+
+import { getDefaultSort, Sort } from '../src/DataTable/DataTable';
+import { DataField, DataRow } from '../src/interfaces/Data';
+
+const defaultData: DataRow[] = [];
+for (let i = 0; i < 100; i++) {
+    const row: DataRow = {
+        data:
+            {
+                company: faker.company.companyName(),
+                phone: faker.phone.phoneNumber(),
+                last_seen: faker.date.between('2015-01-01', '2018-12-31').toDateString()
+            }
+
+    };
+    defaultData.push(row);
+}
+
+const fields: DataField[] = [
+    {
+        type: 'string',
+        name: 'company',
+        hasNegative: false,
+        isDateTime: false,
+        display: 'Company'
+    },
+    {
+        type: 'string',
+        name: 'phone',
+        hasNegative: false,
+        isDateTime: false,
+        display: 'Telephone'
+    },
+    {
+        type: 'string',
+        name: 'last_seen',
+        hasNegative: false,
+        isDateTime: true,
+        display: 'Last seen'
+    }
+];
+
+const columns = [
+    {
+        type: 'SELECT',
+        width: 50
+    },
+    {
+        type: 'DATA',
+        fieldName: 'company',
+        width: 475,
+        sortable: true,
+        defaultSort: true,
+        defaultSortDirection: 'ASC'
+    },
+    {
+        type: 'DATA',
+        fieldName: 'phone',
+        width: 150,
+        align: 'right',
+        sortable: true
+    },
+    {
+        type: 'DATA',
+        fieldName: 'last_seen',
+        width: 150,
+        align: 'right',
+        sortable: true,
+    },
+    {
+        type: 'TOOLBAR',
+        width: 110,
+        toolbar: (row: DataRow) => (
+            <ButtonGroup size={'s'}>
+                <RowAction onClick={() => console.log('Edit', row)}><Edit /></RowAction>
+                <RowAction onClick={() => console.log('Delete', row)}><Trash /></RowAction>
+                <RowAction><Options /></RowAction>
+            </ButtonGroup>
+        )
+    }
+];
+
+const sortData = (data: DataRow[], sort: Sort) => {
+    const reverse = (sort.direction === 'DESC') ? {reverse: true} : null;
+    return arraySort(data, `data.${sort.field.name}`, reverse);
+};
+
+const DataTableWithSort = () => {
+    const defaultSort = getDefaultSort(columns, fields);
+    const sortedData = sortData(defaultData, defaultSort);
+
+    const [data, setData] = React.useState<DataRow[]>(sortedData);
+
+    const sortHandler = (sort: Sort) => {
+        const sortedData = sortData(data, sort);
+        setData(sortedData);
+    };
+
+    return (
+        <DataTable columns={columns} data={data} sortHandler={sortHandler} fields={fields}  />
+    );
+};
+
+const Card = styled.div`
+    width: 100%;
+    height: 200px;
+    box-sizing: border-box;
+    margin: 0 auto;
+    background: #ffffff;
+    box-shadow: 0px 4px 9px rgba(0,0,0,0.02);
+`;
+
+storiesOf('Layout', module)
+.add('Basic', () => {
+    return (
+        <Basic>
+            <Card>
+                <DataTableWithSort />
+            </Card>
+        </Basic>
+    );
+});
