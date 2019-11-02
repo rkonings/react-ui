32615002d59427e727237cf43cb9844a2c592f5f
rkonings <41450820+rkonings@users.noreply.github.com>
2019-11-02 11:49:26 +0100

    feat(storybook): add 100% height to story container, body and html (#57)

--- .storybook/GlobalStyleDecorator.tsx
+++ .storybook/GlobalStyleDecorator.tsx
@@ -3,12 +3,27 @@ import React from 'react';
 
 import GlobalStyles from '../src/GlobalStyle';
 
+import { createGlobalStyle } from 'styled-components';
+
+const StoryStyle = createGlobalStyle`
+    html, body {
+       height: 100%;
+       margin:0;
+    }
+
+    #root {
+      height: 100%;
+      width: 100%;
+    }
+`;
+
 // https://medium.com/@cbovis/using-the-styled-components-createglobalstyle-api-with-react-storybook-e2e64255d567
 
 const GlobalStyleDecorator = (storyFn) => {
   return (
     <React.Fragment>
       <GlobalStyles />
+      <StoryStyle />
       {storyFn()}
     </React.Fragment>
   );
