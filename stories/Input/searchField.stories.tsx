c92c19fec03b321abaf17968de4199eb28e33156
rkonings <randy@randykonings.nl>
2019-11-10 15:45:52 +0100

    use Football API data

--- stories/Input/searchField.stories.tsx
+++ stories/Input/searchField.stories.tsx
@@ -2,45 +2,30 @@ import { storiesOf } from '@storybook/react';
 import Fuse from 'fuse.js';
 import React from 'react';
 import Search from '../../src/Input/SearchField/SearchField';
+import data from './data.json';
 
 storiesOf('Input/SearchField', module)
 .add('default', () => {
-    interface SimpleBook {
-      title: string;
-      author: {
-          firstName: string;
-          lastName: string;
-      };
-      tags: string[];
-  }
 
-  const books: SimpleBook[] = [{
-    title: 'Old Man\'s War',
-    author: {
-      firstName: 'John',
-      lastName: 'Scalzi'
-    },
-    tags: ['fiction']
-  }, {
-    title: 'The Lock Artist',
-    author: {
-      firstName: 'Steve',
-      lastName: 'Hamilton'
-    },
-    tags: ['thriller']
-  }];
+  interface Item {
+      name: string;
+      code: string | null;
+  }
 
   const BookSearchField = () => {
     const [result, setResult] = React.useState<string[]>([]);
 
-    const options: Fuse.FuseOptions<SimpleBook> = {
-        keys: ['author', 'tags', 'title'],
+    const options: Fuse.FuseOptions<Item> = {
+        maxPatternLength: 32,
+        minMatchCharLength: 3,
+        threshold: 0.2,
+        keys: ['name', 'code'],
       };
-    const fuse = new Fuse(books, options);
+    const fuse = new Fuse(data.clubs, options);
 
     const searchHandler = (value: string) => {
         const results = fuse.search(value);
-        setResult(results.map((item) => item.title));
+        setResult(results.map((item) => item.name));
 
     };
     return (<Search result={result} onChange={searchHandler} />);
