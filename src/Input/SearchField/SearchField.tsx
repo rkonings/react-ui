13b5a8f924675724db30cad6cbddb5715772a1f2
rkonings <41450820+rkonings@users.noreply.github.com>
2019-11-11 19:49:13 +0100

    feat(input): add search field (#61)
    * add fuse.js to project for fuzzysearch
    
    * add search field component
    
    * add story
    
    * add debounce
    
    * use Football API data
    
    * add resolveJsonModule option
    
    * add faker data story
    
    * add minimal value length to start searching
    
    * close result menu on outside click
    
    * open result menu on textfield focus
    
    * close result menu on textfield blur
    
    * add delay to close the menu
    
    * fix undesirable closing menu when clicking the input element
    
    * change delay to fix click handler
    
    * add clear icon when typing
    
    * close menu on Tab
    
    * fix debounce
    
    * don't search if changed value is empty
    
    * refactor code
    
    * change component name
    
    * add placeholder
    
    * add close icon

---
+++ src/Input/SearchField/SearchField.tsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Close, Search } from '../../Icon';
+import TextField from '../TextField/TextField';
+
+export const MenuItem = styled.a`
+    padding: 15px 25px;
+    color: ${({theme: { menu: { item } }}) => item.default.color };
+    display: flex;
+    cursor: pointer;
+    font-size: 14px;
+
+    &:hover {
+        ${({theme: { menu: {item: { hover}} }}) => {
+            return `
+                background: ${hover.backgroundColor};
+                color: ${hover.color};
+            `;
+        } }
+    }
+`;
+
+export const Menu = styled.div`
+    ${({theme: { menu}}) => {
+        return `
+            background: ${menu.backgroundColor};
+            box-shadow: ${menu.boxShadow};
+        `;
+    }}
+
+    width: 100%;
+    display:flex;
+    flex-direction: column;
+    min-width: 200px;
+`;
+interface SearchField {
+    className?: string;
+    result?: string[];
+    placeHolder?: string;
+    onChange(searchValue: string, search?: boolean): void;
+}
+
+const StyledClickAway = styled.div`
+    position: fixed;
+    top:0;
+    left:0;
+    bottom:0;
+    right:0;
+`;
+
+const Clear = styled.div``;
+
+const SearchField = ({className, result, onChange, placeHolder}: SearchField) => {
+
+    const [isOpen, setIsOpen] = React.useState<boolean>(true);
+    const [showClear, setShowClear] = React.useState<boolean>(false);
+    const [value, setValue] = React.useState<string>('');
+    const [timeoutId, setTimeoutId] = React.useState<ReturnType<typeof setTimeout> | undefined>();
+
+    React.useEffect(() => {
+        setShowClear(value.length > 0 ? true : false);
+    }, [value]);
+
+    const clear = () => {
+        setIsOpen(false);
+        setValue('');
+        onChange('', false);
+    }
+
+    return (
+        <div className={className}>
+            <TextField
+                onFocus={() => setIsOpen(true)}
+                prefix={<Search/>}
+                onKeyDown={(e) => {
+                    if (e.key === 'Tab') {
+                        setIsOpen(false);
+                    }
+                }}
+                postfix={showClear ? (
+                    <Clear onClick={() => clear()}>
+                        <Close />
+                    </Clear>
+                ) : undefined}
+                value={value}
+                placeHolder={placeHolder}
+                onChange={(e) => {
+                    const newValue = e.currentTarget.value;
+                    setValue(newValue);
+                    if (timeoutId) {
+                        clearTimeout(timeoutId);
+                    }
+                    if (newValue.length > 0) {
+                        setTimeoutId(setTimeout(() => onChange(newValue), 500));
+                    } else {
+                        onChange(newValue, false);
+                    }
+                }}
+            />
+            {result && result.length > 0 && isOpen && (
+                <React.Fragment>
+                    <StyledClickAway onClick={() => setIsOpen(false)} />
+                    <Menu>
+                        {result.map((item) => (
+                            <MenuItem
+                                key={item}
+                                onClick={() => {
+                                    setIsOpen(false);
+                                    setValue(item);
+                                    onChange(item, false);
+                                }}
+                            >
+                                {item}
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                </React.Fragment>
+            )}
+
+        </div>
+    );
+};
+
+export default styled(SearchField)`
+    position: relative;
+    ${TextField} {
+        position: relative;
+        z-index: 1;
+    }
+    ${Menu} {
+        position: absolute;
+        z-index: 999;
+        max-height: 185px;
+        overflow-y: scroll;
+    }
+
+    ${Close} {
+        cursor: pointer;
+    }
+`;
---
+++ stories/Input/searchField.stories.tsx
@@ -0,0 +1,78 @@
+import { storiesOf } from '@storybook/react';
+import faker from 'faker';
+import Fuse from 'fuse.js';
+import React from 'react';
+import Search from '../../src/Input/SearchField/SearchField';
+import data from './data.json';
+
+storiesOf('Input/SearchField', module)
+.add('football data', () => {
+
+  interface Item {
+      name: string;
+      code: string | null;
+  }
+
+  const BookSearchField = () => {
+    const [result, setResult] = React.useState<string[]>([]);
+
+    const options: Fuse.FuseOptions<Item> = {
+        maxPatternLength: 32,
+        minMatchCharLength: 3,
+        threshold: 0.2,
+        keys: ['name', 'code'],
+      };
+    const fuse = new Fuse(data.clubs, options);
+
+    const searchHandler = (value: string) => {
+        const results = fuse.search(value);
+        setResult(results.map((item) => item.name));
+
+    };
+    return (<Search placeHolder="Search football club" result={result} onChange={searchHandler} />);
+  };
+
+  return (
+    <BookSearchField />
+  );
+})
+.add('faker data', () => {
+
+  interface PersonData {
+      name: string;
+  }
+
+  const persons: PersonData[] = [];
+
+  for (let i = 0; i < 1000; i++) {
+    persons.push({
+      name: faker.name.firstName() +  ' ' + faker.name.lastName()
+    });
+  }
+  const PersonSearchField = () => {
+    const [result, setResult] = React.useState<string[]>([]);
+
+    const options: Fuse.FuseOptions<PersonData> = {
+        threshold: 0.1,
+        keys: ['name'],
+      };
+    const fuse = new Fuse(persons, options);
+
+    const searchHandler = (value: string, search: boolean = true) => {
+        if (value.length > 2 && search) {
+          const results = fuse.search(value);
+          setResult(results.map((item) => item.name));
+        } else {
+          setResult([]);
+        }
+
+    };
+    return (<Search placeHolder="Search person" result={result} onChange={searchHandler} />);
+  };
+
+  return (
+    <div>
+      <PersonSearchField />
+    </div>
+  );
+});
