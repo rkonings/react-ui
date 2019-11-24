7a294bd3f3da0377f8f85728a8d97f754f547d64
rkonings <41450820+rkonings@users.noreply.github.com>
2019-11-24 19:32:53 +0100

    chore: move styles (#70)
    * remove style
    
    * add style

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -2,6 +2,16 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+    html, body {
+       height: 100%;
+       margin:0;
+    }
+
+    #root {
+      height: 100%;
+      width: 100%;
+    }
+
     body {
         font-family: 'Roboto', sans-serif;
         -webkit-font-smoothing: antialiased;
