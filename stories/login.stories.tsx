09a4c3488494ea922b24365509c97f09d3ce5347
rkonings <randy@randykonings.nl>
2019-12-03 17:20:26 +0100

    add settings story with react-router decorator

--- stories/login.stories.tsx
+++ stories/login.stories.tsx
@@ -1,20 +1,21 @@
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-
-import CreateAccount from '../src/UI/CreateAccount';
+import StoryRouter from 'storybook-react-router';
 import Login from '../src/UI/Login';
+import CreateAccount from '../src/UI/CreateAccount';
+import Settings from '../src/UI/Settings';
 
 storiesOf('UI/Forms', module)
-.add('login', () => {
+  .add('login', () => {
+    return <Login onLogin={action('login')} />;
+  })
+  .add('Create account', () => {
+    return <CreateAccount onCreate={action('Create account')} />;
+  });
 
-  return (
-    <Login onLogin={action('login')}/>
-  );
-})
-.add('Create account', () => {
-
-  return (
-    <CreateAccount onCreate={action('Create account')}/>
-  );
-});
+storiesOf('UI/Settings', module)
+  .addDecorator(StoryRouter())
+  .add('Settings', () => {
+    return <Settings />;
+  });
