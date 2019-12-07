2b44f9721cab2e44e6163def2b3c944ef675c980
rkonings <41450820+rkonings@users.noreply.github.com>
2019-12-07 14:57:35 +0100

    feat(input): add popup input (#83)
    * add popup input component
    
    * add story

---
+++ src/CombinedInput/PopupInput.tsx
@@ -0,0 +1,129 @@
+import dotProp from 'dot-prop';
+import React from 'react';
+import * as Yup from 'yup';
+import Popup from '../Popup/Popup';
+
+export type ValidationErrors = Map<string, string>;
+export interface ChangedItem {
+    field: string;
+    value: string | boolean | number;
+}
+
+export interface ChangeOptions {
+    saveFields?: boolean;
+}
+export type ChangedItems = ChangedItem[];
+export type OnChangeHandler = (
+    items: ChangedItems,
+    options?: ChangeOptions,
+    callBack?: () => void
+) => void;
+
+export const mapValidationErrors = (error: Yup.ValidationError) => {
+    if (error.inner.length > 0) {
+        return error.inner.reduce(
+            (obj: ValidationErrors, item: Yup.ValidationError) => {
+                return obj.set(item.path, item.message);
+            },
+            new Map()
+        );
+    } else {
+        const errorsMap = new Map<string, string>();
+        errorsMap.set(error.path, error.message);
+        return errorsMap;
+    }
+};
+
+interface PopupInputProps<T> {
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    errors: ValidationErrors;
+    values: T;
+    onChange: (field: string, value: boolean | number | string) => void;
+    onSave: () => void;
+    onCancel: () => void;
+}
+
+interface PopoverInput<T> {
+    onChange: OnChangeHandler;
+    errors?: ValidationErrors;
+    validationSchema: Yup.ObjectSchema;
+    values: T;
+    link: JSX.Element;
+    children: (
+        props: PopupInputProps<T>
+    ) => string | JSX.Element | JSX.Element[];
+}
+
+export const PopupInput = <T extends {}>({
+    children,
+    values,
+    onChange,
+    errors,
+    validationSchema,
+    link,
+}: PopoverInput<T>) => {
+    const [inputValues, setInputValues] = React.useState<T>(values);
+    const [inputErrors, setInputErrors] = React.useState<ValidationErrors>(
+        errors || new Map()
+    );
+
+    React.useEffect(() => {
+        setInputValues({ ...values });
+    }, [values]);
+
+    const onChangeInputField = async (
+        field: string,
+        value: boolean | number | string
+    ) => {
+        const values = { ...inputValues };
+        dotProp.set(values, field, value);
+        setInputValues(values);
+
+        validationSchema
+            .validateAt(field, values)
+            .then(() => {
+                const errors = new Map(inputErrors);
+                errors.delete(field);
+                setInputErrors(errors);
+            })
+            .catch(error => {
+                const errors = mapValidationErrors(error);
+                setInputErrors(errors);
+            });
+    };
+
+    const onSave = (setOpen: React.Dispatch<React.SetStateAction<boolean>>) => {
+        const values = Object.entries(inputValues).map(([key, value]) => ({
+            field: key,
+            value,
+        }));
+        if (inputErrors.size === 0) {
+            onChange(values as ChangedItems, { saveFields: true }, () => {
+                setOpen(false);
+            });
+        }
+    };
+
+    const onCancel = (
+        setOpen: React.Dispatch<React.SetStateAction<boolean>>
+    ) => {
+        setInputValues(values);
+        setOpen(false);
+    };
+    return (
+        <Popup link={link}>
+            {setOpen =>
+                children({
+                    setOpen,
+                    errors: inputErrors,
+                    values: inputValues,
+                    onChange: onChangeInputField,
+                    onSave: () => onSave(setOpen),
+                    onCancel: () => onCancel(setOpen),
+                })
+            }
+        </Popup>
+    );
+};
+
+export default PopupInput;
---
+++ stories/Input/popupInput.stories.tsx
@@ -0,0 +1,87 @@
+import { action } from '@storybook/addon-actions';
+import { boolean, text } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+import faker from 'faker';
+import React from 'react';
+import * as Yup from 'yup';
+import Button from '../../src/Button/Button';
+import TextButton from '../../src/Button/TextButton';
+import ButtonGroup from '../../src/ButtonGroup/ButtonGroup';
+import TextField from '../../src/Input/TextField/TextField';
+
+import styled from 'styled-components';
+import PopupInput from '../../src/CombinedInput/PopupInput';
+import { PopupContent, PopupFooter, PopupHeader } from '../../src/Popup/Popup';
+
+const InputField = styled.div`
+    padding-bottom: 2em;
+    ${TextField} {
+        input {
+            background: none;
+        }
+    }
+`;
+
+storiesOf('Input/Popup', module).add('default', () => {
+    const values = {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+    };
+
+    const validationSchema = Yup.object({
+        firstName: Yup.string().required('is required'),
+        lastName: Yup.string().required('field is required'),
+    });
+
+    return (
+        <PopupInput
+            link={<Button>Popup</Button>}
+            onChange={(values, options, cb) => {
+                cb();
+                action('onChange')(values, options);
+            }}
+            validationSchema={validationSchema}
+            values={values}
+        >
+            {({ setOpen, errors, values, onChange, onSave, onCancel }) => (
+                <React.Fragment>
+                    <PopupHeader>Store client</PopupHeader>
+                    <PopupContent>
+                        <InputField>
+                            <TextField
+                                value={values.firstName}
+                                width="200px"
+                                placeHolder="Firstname"
+                                onChange={e =>
+                                    onChange('firstName', e.currentTarget.value)
+                                }
+                                errorText={errors.get('firstName')}
+                            />
+                        </InputField>
+                        <InputField>
+                            <TextField
+                                value={values.lastName}
+                                width="200px"
+                                placeHolder="LastName"
+                                onChange={e =>
+                                    onChange('lastName', e.currentTarget.value)
+                                }
+                                errorText={errors.get('lastName')}
+                            />
+                        </InputField>
+                    </PopupContent>
+                    <PopupFooter>
+                        <ButtonGroup>
+                            <TextButton onClick={() => onCancel()}>
+                                cancel
+                            </TextButton>
+                            <Button type="primary" onClick={() => onSave()}>
+                                Save
+                            </Button>
+                        </ButtonGroup>
+                    </PopupFooter>
+                </React.Fragment>
+            )}
+        </PopupInput>
+    );
+});
