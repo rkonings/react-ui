8f618c209cb008a29122428a87315ddf10f56920
rkonings <randy@randykonings.nl>
2019-12-09 10:33:23 +0100

    add interfaces

--- src/Form/index.ts
+++ src/Form/index.ts
@@ -1,6 +1,21 @@
 import styled from 'styled-components';
 import TextField from '../Input/TextField/TextField';
 
+export interface ChangedItem {
+    field: string;
+    value: string | boolean | number;
+}
+
+export interface ChangeOptions {
+    saveFields?: boolean;
+}
+export type ChangedItems = ChangedItem[];
+export type OnChangeHandler = (
+    items: ChangedItems,
+    options?: ChangeOptions,
+    callBack?: () => void
+) => void;
+
 export const InputField = styled.div`
     padding-bottom: 2em;
 
