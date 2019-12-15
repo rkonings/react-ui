c9cbea0c0b7b9b2900efec40bd231e94417a9e17
rkonings <randy@randykonings.nl>
2019-12-15 16:32:22 +0100

    add filters

--- src/Filters/FilterPopup.tsx
+++ src/Filters/FilterPopup.tsx
@@ -3,13 +3,35 @@ import styled from 'styled-components';
 import { Button, TextButton } from '../Button';
 import ButtonGroup from '../ButtonGroup/ButtonGroup';
 import Popup, { PopupContent, PopupFooter, PopupHeader } from '../Popup/Popup';
+import { FilterConfig, FilterList } from './Filter';
 
 interface FilterPopup {
     className?: string;
     width?: string;
+    data: FilterConfig[];
+    onChange(values: { [key: string]: string[] }): void;
 }
 
-const FilterPopup = ({ className, width }: FilterPopup) => {
+const FilterPopup = ({ className, width, data, onChange }: FilterPopup) => {
+    const [filters, setFilters] = React.useState<FilterConfig[]>(data);
+    const setFilterValue = (id: string, value: string[]) => {
+        const newFilters = [...filters];
+        const filter = newFilters.find(filter => filter.id === id);
+        if (filter) {
+            filter.value = value;
+            setFilters(newFilters);
+        }
+    };
+    const onChangeHandler = () => {
+        const newValues = filters.reduce((obj, item) => {
+            if (item.value && item.value.length > 0) {
+                obj[item.id] = item.value;
+            }
+            return obj;
+        }, {});
+        onChange(newValues);
+    };
+
     return (
         <div className={className}>
             <Popup
@@ -21,7 +43,20 @@ const FilterPopup = ({ className, width }: FilterPopup) => {
                     <React.Fragment>
                         <PopupHeader>What do u want to filter?</PopupHeader>
                         <PopupContent>
-                            Are u sure to save this client?
+                            {filters.map(filter => {
+                                return (
+                                    <FilterList
+                                        label={filter.label}
+                                        search={filter.search}
+                                        key={filter.id}
+                                        value={filter.value ? filter.value : []}
+                                        options={filter.options}
+                                        onChange={value =>
+                                            setFilterValue(filter.id, value)
+                                        }
+                                    />
+                                );
+                            })}
                         </PopupContent>
                         <PopupFooter>
                             <ButtonGroup>
@@ -30,7 +65,10 @@ const FilterPopup = ({ className, width }: FilterPopup) => {
                                 </TextButton>
                                 <Button
                                     type="primary"
-                                    onClick={() => setOpen(false)}
+                                    onClick={() => {
+                                        onChangeHandler();
+                                        setOpen(false);
+                                    }}
                                 >
                                     Filter
                                 </Button>
@@ -43,4 +81,13 @@ const FilterPopup = ({ className, width }: FilterPopup) => {
     );
 };
 
-export default styled(FilterPopup)``;
+export default styled(FilterPopup)`
+    ${PopupContent} {
+        flex-direction: row;
+        display: flex;
+    }
+
+    ${FilterList} {
+        margin-right: 3em;
+    }
+`;
