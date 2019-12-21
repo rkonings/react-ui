fb611100a88c16752f2d7d1ecb893ba16e7b3b94
rkonings <randy@randykonings.nl>
2019-12-21 16:22:45 +0100

    add InlineEditableTextField

---
+++ src/Input/InlineEditable/InlineEditableTextField.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import TextField, { Label } from '../TextField/TextField';
+
+export const InlineEditableTextField = styled(TextField)`
+    input {
+        padding: 0.5em 1em 0.5em 0;
+    }
+
+    label {
+        border-bottom-color: rgba(0, 0, 0, 0);
+    }
+
+    ${Label} {
+        font-weight: 300;
+        ${({ theme: { color } }) => color.gray60};
+        font-size: 80%;
+    }
+`;
