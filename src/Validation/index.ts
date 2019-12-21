a5a508e1bad4b5ead7ab0dad3d5f04c31afc0be7
rkonings <randy@randykonings.nl>
2019-12-21 16:22:07 +0100

    add mapValidationErrors to validation file

--- src/Validation/index.ts
+++ src/Validation/index.ts
@@ -1,3 +1,18 @@
 import * as Yup from 'yup';
 export { Yup };
 export type ValidationErrors = Map<string, string>;
+
+export const mapValidationErrors = (error: Yup.ValidationError) => {
+    if (error.inner.length > 0) {
+        return error.inner.reduce(
+            (obj: ValidationErrors, item: Yup.ValidationError) => {
+                return obj.set(item.path, item.message);
+            },
+            new Map()
+        );
+    } else {
+        const errorsMap = new Map<string, string>();
+        errorsMap.set(error.path, error.message);
+        return errorsMap;
+    }
+};
