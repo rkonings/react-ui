67101b643e5b2266c85c2d281d4e1a7bb6e6fbd9
rkonings <randy@randykonings.nl>
2019-12-24 11:06:32 +0100

    add new type option

--- src/Tab/Tab.tsx
+++ src/Tab/Tab.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
+type TabType = 'minimal' | 'default';
+
 interface Tab {
     className?: string;
     children: JSX.Element[] | JSX.Element;
     active: string;
+    type?: TabType;
 }
 
 interface TabContent {
@@ -16,14 +19,14 @@ export const TabContent = styled.div<TabContent>`
     padding: 2em 1em;
 `;
 
-const TabNavigation = styled.div`
+const TabNavigation = styled.div<{ type?: TabType }>`
     width: 100%;
     display: flex;
     flex-direction: row;
     border-bottom: 1px solid ${({ theme: { color } }) => color.gray50};
     height: 51px;
 `;
-const TabNavigationItem = styled.div<{ active?: boolean }>`
+const TabNavigationItem = styled.div<{ active?: boolean; type?: TabType }>`
     cursor: pointer;
     min-width: 120px;
     padding: 0 1em;
@@ -33,19 +36,40 @@ const TabNavigationItem = styled.div<{ active?: boolean }>`
     font-size: 13px;
     text-align: center;
     height: 50px;
-    border: 1px solid ${({ theme: { color } }) => color.gray50};
-    border-left: 0;
-    ${({ active }) =>
-        active ? 'border-bottom: 1px solid #ffffff;' : 'border-bottom: 0;'};
-    background: ${({ active, theme: { color } }) =>
-        active ? '#ffffff' : color.gray10};
 
-    &:first-child {
-        border-left: 1px solid ${({ theme: { color } }) => color.gray50};
-    }
+    ${({ type, theme: { color }, active }) => {
+        if (type === 'minimal') {
+            const borderBottom = active
+                ? `border-bottom: 5px solid ${color.primary};`
+                : 'border-bottom: 0;';
+
+            const textColor = active ? `color: ${color.primary}` : '';
+            return `
+                ${textColor};
+                ${borderBottom}
+            `;
+        } else {
+            const isActive = active
+                ? 'border-bottom: 1px solid #ffffff;'
+                : 'border-bottom: 0;';
+
+            const background = active ? '#ffffff' : color.gray10;
+
+            return `
+                border: 1px solid ${color.gray50};
+                border-left: 0;
+                ${isActive}
+                background: ${background};
+
+                &:first-child {
+                    border-left: 1px solid ${color.gray50};
+                }
+            `;
+        }
+    }}
 `;
 
-const Tab = ({ className, children, active }: Tab) => {
+const Tab = ({ className, children, active, type = 'default' }: Tab) => {
     const [activeTab, setActiveTab] = React.useState<string>();
 
     React.useEffect(() => {
@@ -54,10 +78,11 @@ const Tab = ({ className, children, active }: Tab) => {
 
     return (
         <div className={className}>
-            <TabNavigation>
+            <TabNavigation type={type}>
                 {React.Children.map(children, child => {
                     return (
                         <TabNavigationItem
+                            type={type}
                             active={child.props.id === activeTab}
                             onClick={() => setActiveTab(child.props.id)}
                         >
