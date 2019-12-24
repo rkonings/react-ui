11cc0ef4db8d7a6ab5ab45e1e231bf297ddcdd59
rkonings <randy@randykonings.nl>
2019-12-24 10:43:23 +0100

    fix lint errors

--- stories/tab.stories.tsx
+++ stories/tab.stories.tsx
@@ -1,22 +1,24 @@
+import { select } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import styled from 'styled-components';
-import { select } from '@storybook/addon-knobs';
 
-import Tab, { TabContent } from '../src/Tab/Tab';
-import {Button} from '../src/Button';
+import { Button } from '../src/Button';
 import Checkbox from '../src/Input/Checkbox/Checkbox';
-import Select from '../src/Input/Select/Select';
 import Switch from '../src/Input/Switch/Switch';
 import { Section, SettingsField, Title } from '../src/SettingsField';
+import Tab, { TabContent } from '../src/Tab/Tab';
 
 const Wrapper = styled.div`
     width: 60%;
 `;
 
-storiesOf('Tabs', module)
-.add('default', () => {
-    const activeTabId = select('activeTabId', ['tab-1', 'tab-2', 'tab-3'], 'tab-1');
+storiesOf('Tabs', module).add('default', () => {
+    const activeTabId = select(
+        'activeTabId',
+        ['tab-1', 'tab-2', 'tab-3'],
+        'tab-1'
+    );
     return (
         <Wrapper>
             <Tab active={activeTabId}>
