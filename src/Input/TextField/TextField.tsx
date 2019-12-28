155e73bcc34fe3cbde81c682275e204bec08ee41
rkonings <randy@randykonings.nl>
2019-12-28 20:30:14 +0100

    use input interfaces, types and elements

--- src/Input/TextField/TextField.tsx
+++ src/Input/TextField/TextField.tsx
@@ -1,119 +1,24 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import Theme from '../../interfaces/Theme';
-import { Size } from '../../interfaces/Theme';
+import {
+    BaseStyle,
+    InputProps,
+    Label,
+    Postfix,
+    Prefix,
+    Style,
+    Wrapper,
+} from '../';
 import { ErrorText, HelperText } from '../Core';
 
 const DEFAULT_TYPE = 'text';
-type TextFieldStyle = 'default' | 'outlined';
 
-export interface TextFieldProps extends HelperText, ErrorText {
-    inputType?: string;
-    className?: string;
-    placeHolder?: string;
-    value?: string | number;
-    width?: string;
-    disabled?: boolean;
-    readOnly?: boolean;
-    autoFocus?: boolean;
-    required?: boolean;
-    theme: Theme;
-    textAlign?: 'left' | 'right';
-    name?: string;
-    size?: Size;
-    grow?: boolean;
-    label?: string | JSX.Element;
-    prefix?: string | JSX.Element;
-    postfix?: string | JSX.Element;
-    style?: TextFieldStyle;
+export interface TextFieldProps extends InputProps, HelperText, ErrorText {
     onChange?(e: React.FormEvent<HTMLInputElement>): void;
     onBlur?(e: React.FormEvent<HTMLInputElement>): void;
     onFocus?(e: React.FormEvent<HTMLInputElement>): void;
     onKeyDown?(e: React.KeyboardEvent<HTMLInputElement>): void;
 }
-
-const Prefix = styled.span`
-    display: flex;
-    align-items: center;
-    margin-right: 5px;
-`;
-
-const Postfix = styled.span`
-    display: flex;
-    align-items: center;
-    margin-left: 5px;
-`;
-
-export const Label = styled.span`
-    display: block;
-    width: 100%;
-    font-weight: 500;
-`;
-
-interface Label {
-    focus: boolean;
-    disabled?: boolean;
-    errorText?: string;
-    _style?: TextFieldStyle;
-}
-const Wrapper = styled.label<Label>`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: center;
-    transition: border 500ms ease-out;
-
-    ${({
-        theme: {
-            input: { textField, error },
-        },
-        errorText = false,
-        disabled,
-        _style = 'default',
-    }) => {
-        let border = 'border-bottom';
-        if (_style === 'outlined') {
-            border = 'border';
-        }
-
-        const borderColor = errorText
-            ? error.color
-            : textField.default.hover.borderColor;
-
-        return disabled
-            ? null
-            : `
-            &:hover {
-                ${border}: ${textField.borderSize} solid ${borderColor};
-                color: ${textField.default.hover.color};
-            }
-        `;
-    }}
-
-    ${({
-        theme: {
-            input: { textField, error },
-        },
-        errorText = false,
-        focus,
-        _style = 'default',
-    }) => {
-        const type = 'default';
-        const state = focus ? 'focus' : 'default';
-        const borderColor = errorText
-            ? error.color
-            : textField[type][state].borderColor;
-
-        let border = 'border-bottom';
-        if (_style === 'outlined') {
-            border = 'border';
-        }
-
-        return `
-            ${border}: ${textField.borderSize} solid ${borderColor};
-        `;
-    }}
-`;
 
 const TextField = ({
     className,
@@ -189,103 +94,9 @@ const TextField = ({
     );
 };
 
-const BaseStyle = ({
-    theme: {
-        input: { textField },
-    },
-    width: _width = '300px',
-    grow: _grow = false,
-    textAlign = 'left',
-    prefix,
-    postfix,
-    size = 'm',
-    style = 'default',
-}: TextFieldProps) => {
-    const type = 'default';
-    const grow = _grow ? 'flex: 1;' : '';
-    const width = !_grow ? 'width: ' + _width + ';' : '';
-    return `
-        box-sizing: border-box;
-        ${width}
-        ${grow}
-        font-size: ${textField.size[size]}px;
-
-        input {
-            text-align: ${textAlign};
-            display: flex;
-            flex: 1;
-            width: 100%;
-            box-sizing: border-box;
-            font-size: ${textField.size[size]}px;
-            padding: ${style === 'default' ? '1em 1em 1em 0' : '1em'};
-            ${postfix ? 'padding-right: 0' : ''};
-            ${prefix ? 'padding-left: 0' : ''};
-
-            &::-webkit-input-placeholde { color: ${
-                textField[type].default.placeholderColor
-            }; }
-            &::-moz-placeholder { color: ${
-                textField[type].default.placeholderColor
-            }; }
-            &:-ms-input-placeholder { color: ${
-                textField[type].default.placeholderColor
-            }; }
-            &::placeholder {
-                transition : opacity 200ms ease-out;
-                opacity:1;
-                color: ${textField[type].default.placeholderColor};
-            }
-            &::-ms-input-placeholder { color: ${
-                textField[type].default.placeholderColor
-            }; }
-        }
-
-    `;
-};
-
 const StyledTextField = styled(TextField)`
     ${BaseStyle};
-
-    ${({
-        style = 'default',
-        label,
-        theme: {
-            input: { textField },
-        },
-    }) => {
-        const type = 'default';
-        const marginTop = style === 'default' || !label ? '0' : '1em';
-        return `
-
-            ${Wrapper} {
-                margin-top: ${marginTop}
-
-            }
-
-            input {
-
-                &:read-only {
-                    cursor: default;
-                }
-
-                border: none;
-                color: ${textField[type].default.color};
-
-                &:focus {
-
-                    color: ${textField[type].focus.color};
-                    outline: none;
-
-                    &::-webkit-input-placeholde { opacity: 0;}
-                    &::-moz-placeholder { opacity: 0;}
-                    &:-ms-input-placeholder { opacity: 0;}
-                    &::placeholder { opacity: 0;}
-                    &::-ms-input-placeholder { opacity: 0;}
-                }
-            }
-
-        `;
-    }};
+    ${Style};
 `;
 
 export default StyledTextField;
