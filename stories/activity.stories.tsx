f4af5fa5f19b28e51e65b6bab77fa43863492743
rkonings <41450820+rkonings@users.noreply.github.com>
2019-12-29 12:53:12 +0100

    chore(activity): refactor naming and fix all lint errors (#106)

--- stories/activity.stories.tsx
+++ stories/activity.stories.tsx
@@ -1,10 +1,9 @@
-import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
 import React from 'react';
 import styled from 'styled-components';
-import Activity from '../src/Activity/Activity';
-import UpdateActivity from '../src/Activity/UpdateActivity';
 import * as Yup from 'yup';
+import Activity from '../src/Activity/Activity';
 
 const Wrapper = styled.div`
     width: 500px;
@@ -19,42 +18,27 @@ const validationSchema = Yup.object({
     type: Yup.string().required('type is required'),
 });
 
-storiesOf('Activity', module)
-    .add('default', () => {
-        const activity = {
-            type: 'call',
-            title: 'rem neque laborum consequuntur',
-            notes:
-                'Est ex et velit quas reprehenderit nesciunt.\nRepellendus mollitia tempora explicabo iure fugiat omnis harum accusamus ad.\nImpedit magni cupiditate ut.\nMinima qui cupiditate.\nQuaerat eum veritatis.',
-            creationDate: new Date(1552769616138),
-        };
+storiesOf('Activity', module).add('default', () => {
+    const activity = {
+        _id: 'FOO',
+        type: 'call',
+        title: 'rem neque laborum consequuntur',
+        notes:
+            'Est ex et velit quas reprehenderit nesciunt.\nRepellendus mollitia tempora explicabo iure fugiat omnis harum accusamus ad.\nImpedit magni cupiditate ut.\nMinima qui cupiditate.\nQuaerat eum veritatis.',
+        creationDate: new Date(1552769616138),
+    };
 
-        return (
-            <Wrapper>
-                <Activity activity={activity} />
-            </Wrapper>
-        );
-    })
-    .add('update', () => {
-        const activity = {
-            type: 'call',
-            title: 'rem neque laborum consequuntur',
-            notes:
-                'Est ex et velit quas reprehenderit nesciunt.\nRepellendus mollitia tempora explicabo iure fugiat omnis harum accusamus ad.\nImpedit magni cupiditate ut.\nMinima qui cupiditate.\nQuaerat eum veritatis.',
-            creationDate: new Date(1552769616138),
-        };
-
-        return (
-            <Wrapper>
-                <UpdateActivity
-                    validationSchema={validationSchema}
-                    activity={activity}
-                    onRemove={action('remove')}
-                    onChange={(items, options, callBack) => {
-                        callBack();
-                        action('onChange')(items, options);
-                    }}
-                />
-            </Wrapper>
-        );
-    });
+    return (
+        <Wrapper>
+            <Activity
+                validationSchema={validationSchema}
+                activity={activity}
+                onRemove={action('remove')}
+                onChange={(items, options, callBack) => {
+                    callBack();
+                    action('onChange')(items, options);
+                }}
+            />
+        </Wrapper>
+    );
+});
