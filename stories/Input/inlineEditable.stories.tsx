caeabd0591363e75fa875adfbb2ca3911c3ace6f
rkonings <randy@randykonings.nl>
2020-01-06 15:47:54 +0100

    add story and add date to schema

--- stories/Input/inlineEditable.stories.tsx
+++ stories/Input/inlineEditable.stories.tsx
@@ -1,40 +1,58 @@
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
+import moment from 'moment';
 import React from 'react';
 import styled from 'styled-components';
 import * as Yup from 'yup';
+import InlineEditableDate from '../../src/Input/InlineEditable/InlineEditableDate';
 import InlineEditableText from '../../src/Input/InlineEditable/InlineEditableText';
-
 const validationSchema = Yup.object({
     title: Yup.string().required('is required'),
+    date: Yup.date(),
 });
 
 const Wrapper = styled.div`
     width: 400px;
 `;
 
-storiesOf('Input/InlineEditable', module).add('default', () => {
-    return (
-        <Wrapper>
-            <InlineEditableText
-                field="title"
+storiesOf('Input/InlineEditable', module)
+    .add('no date', () => {
+        return (
+            <InlineEditableDate
+                field="date"
                 onChange={action('onChange')}
                 validationSchema={validationSchema}
-                value="FOO is not BAZ or FOOBAR"
             />
-            <InlineEditableText
-                field="title"
-                onChange={action('onChange')}
-                validationSchema={validationSchema}
-                value="FOO is not BAZ or FOOBAR"
-                type="TEXTAREA"
-            />
-            <InlineEditableText
-                field="title"
-                onChange={action('onChange')}
-                validationSchema={validationSchema}
-                value="FOO is not BAZ or FOOBAR"
-            />
-        </Wrapper>
-    );
-});
+        );
+    })
+    .add('default', () => {
+        return (
+            <Wrapper>
+                <InlineEditableDate
+                    field="date"
+                    onChange={action('onChange')}
+                    validationSchema={validationSchema}
+                    value={moment()}
+                />
+                <InlineEditableText
+                    field="title"
+                    onChange={action('onChange')}
+                    validationSchema={validationSchema}
+                    value="FOO is not BAZ or FOOBAR"
+                />
+                <InlineEditableText
+                    field="title"
+                    onChange={action('onChange')}
+                    validationSchema={validationSchema}
+                    value="FOO is not BAZ or FOOBAR"
+                    type="TEXTAREA"
+                />
+                <InlineEditableText
+                    field="title"
+                    onChange={action('onChange')}
+                    validationSchema={validationSchema}
+                    value="FOO is not BAZ or FOOBAR"
+                />
+            </Wrapper>
+        );
+    });
