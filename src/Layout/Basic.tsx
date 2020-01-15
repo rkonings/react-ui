6066ee37b9c37bab11c83f336ef716774a547fc8
rkonings <randy@randykonings.nl>
2020-01-15 10:17:56 +0100

    add header tool bar section and remove back component

--- src/Layout/Basic.tsx
+++ src/Layout/Basic.tsx
@@ -9,6 +9,7 @@ interface Basic {
     children?: JSX.Element | JSX.Element[];
     left: JSX.Element;
     pageTitle?: JSX.Element | string;
+    toolbar?: JSX.Element | string;
 }
 
 const Left = styled(Item)`
@@ -24,39 +25,48 @@ const Main = styled.div`
     flex-direction: column;
 `;
 
-const Back = styled.div`
-    font-size: 12px;
-    color: #1b2327;
-    font-weight: 400;
-`;
-
 const Title = styled.h1`
     font-size: 22px;
     color: #1b2327;
     font-weight: 400;
 `;
 
-const Basic = styled(({ className, children, left, pageTitle }: Basic) => {
-    return (
-        <div className={className}>
-            <AppBar>
-                <AppBarTitle>ZZP Online</AppBarTitle>
-            </AppBar>
-            <Grid height="calc(100% - 50px)" width="100%">
-                <Left height="100%" width="200px">
-                    {left}
-                </Left>
-                <Item height="100%" width="calc(100% - 200px)">
-                    <Main>
-                        <Back>Back to dashboard</Back>
-                        {pageTitle && <Title>{pageTitle}</Title>}
-                        {children}
-                    </Main>
-                </Item>
-            </Grid>
-        </div>
-    );
-})`
+const Header = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 98%;
+    justify-content: space-between;
+`;
+const HeaderToolbar = styled.div``;
+
+const Basic = styled(
+    ({ className, children, left, pageTitle, toolbar }: Basic) => {
+        return (
+            <div className={className}>
+                <AppBar>
+                    <AppBarTitle>ZZP Online</AppBarTitle>
+                </AppBar>
+                <Grid height="calc(100% - 50px)" width="100%">
+                    <Left height="100%" width="200px">
+                        {left}
+                    </Left>
+                    <Item height="100%" width="calc(100% - 200px)">
+                        <Main>
+                            <Header>
+                                {pageTitle && <Title>{pageTitle}</Title>}
+                                {toolbar && (
+                                    <HeaderToolbar>{toolbar}</HeaderToolbar>
+                                )}
+                            </Header>
+                            {children}
+                        </Main>
+                    </Item>
+                </Grid>
+            </div>
+        );
+    }
+)`
     width: 100%;
     height: 100%;
     background: #fffcfc;
