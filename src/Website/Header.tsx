2dfb85251124235a151f6e619f9277f2f0f26711
rkonings <randy@randykonings.nl>
2020-02-08 10:43:51 +0100

    add mobile navigation

--- src/Website/Header.tsx
+++ src/Website/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import { device } from '../Device';
+import { MainNavigation, MobileNavigation, NavigationToggle } from './';
+import { usePortal } from '../Portal/Portal';
 
 interface Header {
     className?: string;
     logo: JSX.Element | string;
-    children: JSX.Element;
+    children: JSX.Element | JSX.Element[];
+    mobileNavigation: JSX.Element;
 }
 
 const Logo = styled.div`
@@ -25,25 +28,60 @@ const Navigation = styled.div`
     flex-grow: 1;
     height: 100%;
     align-items: center;
-    justify-content: flex-end;
     padding: 0 2em;
+`;
+
+const ContactInfo = styled.div`
+    display: flex;
+    align-self: center;
+    margin: 2em;
+    padding: 0.8em 1em;
+    font-weight: 800;
+
+    ${({ theme: { color } }) => {
+        return `
+          background: ${color.black};
+          color: ${color.white};
+      `;
+    }}
 
     @media ${device.tablet} {
         display: none;
     }
 `;
 
-export const Header = styled(({ className, logo, children }: Header) => {
-    return (
-        <div className={className}>
-            <Logo>{logo}</Logo>
-            <Navigation>{children}</Navigation>
-        </div>
-    );
-})`
+export const Header = styled(
+    ({ className, logo, children, mobileNavigation }: Header) => {
+        const [navOpen, setNavOpen] = React.useState<boolean>(false);
+        const { Portal } = usePortal();
+
+        return (
+            <div className={className}>
+                <Logo>{logo}</Logo>
+                <Navigation>
+                    <MainNavigation>{children}</MainNavigation>
+                </Navigation>
+                <NavigationToggle
+                    open={navOpen}
+                    onClick={() => setNavOpen(!navOpen)}
+                />
+                {navOpen && (
+                    <Portal onClose={() => setNavOpen(false)}>
+                        <MobileNavigation>{mobileNavigation}</MobileNavigation>
+                    </Portal>
+                )}
+                {/* <ContactInfo>+31 (020) - 4023777</ContactInfo> */}
+            </div>
+        );
+    }
+)`
     display: flex;
     width: 100%;
     height: 100px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
 
     flex-direction: row;
     justify-content: center;
--- src/Website/MainNavigation.tsx
+++ src/Website/MainNavigation.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { device } from '../Device';
+import { Bars, Close } from '../Icon';
+
 interface MainNavItem {
     className?: string;
     children: string | JSX.Element;
@@ -30,6 +33,82 @@ export const MainNavItem = styled(
     }
 `;
 
+interface NavigationToggle {
+    className?: string;
+    onClick: () => void;
+    open: boolean;
+}
+
+export const NavigationToggle = styled(
+    ({ className, onClick, open }: NavigationToggle) => {
+        return (
+            <div onClick={onClick} className={className}>
+                {open ? <Close /> : <Bars />}
+            </div>
+        );
+    }
+)`
+    display: none;
+    padding: 1em;
+    align-self: center;
+
+    @media ${device.tablet} {
+        display: block;
+    }
+`;
+
+interface MobileNavigation {
+    className?: string;
+    children: JSX.Element | JSX.Element[];
+}
+
+export const MobileNavigation = styled(
+    ({ className, children }: MobileNavigation) => {
+        return (
+            <div className={className}>
+                <ul>{children}</ul>
+            </div>
+        );
+    }
+)`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    position: fixed;
+    top: 50px;
+    left: 0;
+    background: #ffffff;
+    padding: 2em;
+    z-index: 9999;
+
+    ul {
+        display: flex;
+        flex-direction: column;
+    }
+
+    ${({ theme: { color } }) => {
+        return `
+          background: ${color.black};
+          color: ${color.white};
+      `;
+    }}
+
+    ${MainNavItem} {
+        a {
+            font-size: 22px;
+            font-weight: 300;
+            display: block;
+            padding: 0.5em 0;
+            ${({ theme: { color } }) => {
+                return `
+                  color: ${color.white};
+              `;
+            }}
+        }
+    }
+`;
+
 interface MainNavigation {
     className?: string;
     children: JSX.Element | JSX.Element[];
@@ -42,4 +121,8 @@ export const MainNavigation = styled(
 )`
     display: flex;
     flex-direction: row;
+
+    @media ${device.tablet} {
+        display: none;
+    }
 `;
