3abf9f87c66a0190633b15d3b4518bf93ee43062
rkonings <randy@randykonings.nl>
2020-02-20 13:32:35 +0100

    fix lint

--- stories/website/elements.stories.tsx
+++ stories/website/elements.stories.tsx
@@ -1,6 +1,6 @@
+import { text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import { text } from '@storybook/addon-knobs';
 
 import { Card } from '../../src/Website/Elements/Card';
 
