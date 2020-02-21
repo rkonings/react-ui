29f5de95ab99ffbfc0dffec967f650b8f673ee23
rkonings <randy@randykonings.nl>
2020-02-21 08:07:50 +0100

    add new style

--- src/Website/Elements/Speciality.tsx
+++ src/Website/Elements/Speciality.tsx
@@ -65,5 +65,28 @@ export const Speciality = styled(({ className, children }: Speciality) => {
 
     @media ${device.tablet} {
         display: block;
+        width: 100%;
+    }
+`;
+
+export const Speciality2 = styled(({ className, children }: Speciality) => {
+    return <div className={className}>{children}</div>;
+})`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+
+    flex-wrap: wrap;
+    max-width: 1000px;
+
+    ${Card} {
+        margin: 2px;
+        height: 170px;
+    }
+
+    @media ${device.tablet} {
+        display: block;
+        width: 100%;
     }
 `;
