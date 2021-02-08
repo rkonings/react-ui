1157cf9c5a29e5bc9a391e80b9c4694dc0e1aa82
Randy Konings <randy.konings@divotion.com>
2021-02-08 18:09:34 +0100

    fix typo

--- src/Layout/index.ts
+++ src/Layout/index.ts
@@ -1,3 +1,3 @@
 export { default as Basic } from './Basic';
 export { default as Detail } from './Detail';
-export { default as Dashbaord } from './Dashboard';
+export { default as Dashboard } from './Dashboard';
